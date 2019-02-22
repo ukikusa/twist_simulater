702fdd34968f97a7e32069b99df48c95969f79b0
kenya <47313624+k-himeno@users.noreply.github.com>
2019-02-22 17:20:05 +0900

    logisticモデルでrを変化させる

---
+++ logistics_frond.py
@@ -0,0 +1,48 @@
+# -*-coding: utf-8 -*-
+"""logistics growth."""
+
+import os
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def logistic(t, N0, K, r):
+    """ロジスティック方程式
+
+    Args:
+        t: 計算する配列．
+        N0: 初期値
+        K: 密度効果
+        r: 増殖率
+    """
+    t = np.array(t)
+    N = K / ((1 + K / N0 - 1) * np.exp(-r * t) + 1)
+    return N
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.join('/hdd1', 'Users', 'kenya', 'Labo', 'keisan', 'python'))
+    # カレントディレクトリの変更．
+
+    time = np.arange(0, 168, 1)
+    N0 = 50
+    K = 4000
+    rs = np.arange(0.04, 0.105, 0.005)
+
+    save = os.path.join('result', 'frond_area', 'N0-' + str(N0) + '_K-' + str(K))
+    if os.path.exists(save) is False:
+        os.makedirs(save)
+    label = []
+    Ns = np.empty((rs.size, time.size), np.uint16)
+    for i, r in enumerate(rs):
+        label.append('r-' + "{0:.3f}".format(r))
+        Ns[i] = logistic(time, N0=N0, K=K, r=r)
+    data = pd.DataFrame(data=Ns, index=label, columns=time)
+    plt.figure()
+    data.T.plot()
+    plt.savefig(os.path.join(save, 'logistic_N0-' + str(N0) + '_K-' + str(K) + '.pdf'))
+    plt.close()
+    data.to_csv(os.path.join(save, 'logistic_N0-' + str(N0) + '_K-' + str(K) + '.csv'))
