28607629aa84caf843c66ae656de15d2e1e67bb5
kenya <47313624+k-himeno@users.noreply.github.com>
2019-02-25 18:21:09 +0900

    Kを変化させる

--- logistics_frond.py
+++ logistics_frond.py
@@ -23,16 +23,7 @@ def logistic(t, N0, K, r):
     return N
 
 
-if __name__ == "__main__":
-    os.chdir(os.path.join('/hdd1', 'Users', 'kenya', 'Labo', 'keisan', 'python'))
-    # カレントディレクトリの変更．
-
-    time = np.arange(0, 168, 1)
-    N0 = 50
-    K = 4000
-    rs = np.arange(0.04, 0.105, 0.005)
-
-    save = os.path.join('result', 'frond_area', 'N0-' + str(N0) + '_K-' + str(K))
+def logistic_rs(time, N0, K, rs, save):
     if os.path.exists(save) is False:
         os.makedirs(save)
     label = []
@@ -46,3 +37,39 @@ if __name__ == "__main__":
     plt.savefig(os.path.join(save, 'logistic_N0-' + str(N0) + '_K-' + str(K) + '.pdf'))
     plt.close()
     data.to_csv(os.path.join(save, 'logistic_N0-' + str(N0) + '_K-' + str(K) + '.csv'))
+    return data
+
+
+def logistic_Ks(time, N0, Ks, r, save):
+    if os.path.exists(save) is False:
+        os.makedirs(save)
+    label = []
+    Ns = np.empty((Ks.size, time.size), np.uint16)
+    for i, K in enumerate(Ks):
+        label.append('K-' + str(K))
+        Ns[i] = logistic(time, N0=N0, K=K, r=r)
+    data = pd.DataFrame(data=Ns, index=label, columns=time)
+    plt.figure()
+    data.T.plot()
+    plt.savefig(os.path.join(save, 'logistic_N0-' + str(N0) + '_r-' + str(r) + '.pdf'))
+    plt.close()
+    data.to_csv(os.path.join(save, 'logistic_N0-' + str(N0) + '_r-' + str(r) + '.csv'))
+    return data
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.join('/hdd1', 'Users', 'kenya', 'Labo', 'keisan', 'python'))
+    # カレントディレクトリの変更．
+
+    time = np.arange(0, 168, 1)
+    N0 = 50
+    K = 4000
+    rs = np.arange(0.04, 0.105, 0.005)
+
+    save = os.path.join('result', 'frond_area', 'N0-' + str(N0) + '_K-' + str(K))
+    logistic_rs(time, N0, K, rs, save)
+
+    r = 0.06
+    Ks = np.arange(2500, 5000, 500, dtype=np.uint16)
+    save = os.path.join('result', 'frond_area', 'N0-' + str(N0) + '_r-' + str(r))
+    logistic_Ks(time, N0, Ks, r, save)
