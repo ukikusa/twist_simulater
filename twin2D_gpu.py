f87dd92e9046c1a231be3c05b6151027762f51e5
kenya <47313624+k-himeno@users.noreply.github.com>
2019-07-23 12:41:10 +0900

    fix make_colour bag

--- twin2D_gpu.py
+++ twin2D_gpu.py
@@ -3,7 +3,8 @@
     Twist chain oscillator simulations. Version using GPU.
 
 It is based on Poincare oscillators.
-Same model as proposed by {Myung, Jihwan, et al. "The choroid plexus is an important circadian clock component." Nature communications 9.1 (2018): 1062.}
+Same model as proposed by 
+{Myung, Jihwan, et al.Nature communications 9.1 (2018): 1062.}
 """
 
 # import sys
@@ -13,9 +14,10 @@ import os
 import chainer.functions
 import cupy as cp
 import cv2
-import image_analysis as im
 import numpy as np
 import pandas as pd
+
+import image_analysis as im
 
 
 def oscillator_2D(x, y, kernel, omega, lambda_, epsilon, A=1, mask=False):
@@ -36,8 +38,12 @@ def oscillator_2D(x, y, kernel, omega, lambda_, epsilon, A=1, mask=False):
     r = cp.linalg.norm(cp.stack([x, y]), axis=0)
     r_shape0, r_shape1 = r.shape
     p = int(kernel.shape[2] * 0.5)
-    field_x = chainer.functions.convolution_2d(x.reshape(1, 1, r_shape0, r_shape1), kernel, pad=p)
-    field_y = chainer.functions.convolution_2d(y.reshape(1, 1, r_shape0, r_shape1), kernel, pad=p)
+    field_x = chainer.functions.convolution_2d(
+        x.reshape(1, 1, r_shape0, r_shape1), kernel, pad=p
+    )
+    field_y = chainer.functions.convolution_2d(
+        y.reshape(1, 1, r_shape0, r_shape1), kernel, pad=p
+    )
     field_x = cp.array(field_x.array, dtype=cp.float64).reshape(r.shape)
     field_y = cp.array(field_y.array, dtype=cp.float64).reshape(r.shape)
     if mask is not False:
@@ -48,15 +54,12 @@ def oscillator_2D(x, y, kernel, omega, lambda_, epsilon, A=1, mask=False):
     return fx, fy
 
 
-def RK4(x, y, kernel, omega, lambda_, epsilon, A, h, mask=False):
+def RK4(x, y, h, **kwargs):
     """Solve oscillator_2D by Runge–Kutta method.
 
     Args:
         x: array of x coordinates
         y: array of y coordinates
-        kernel: [description]
-        omega: angular velocity
-        A: Standard state amplitude
         h: Step width
 
     Returns:
@@ -64,23 +67,22 @@ def RK4(x, y, kernel, omega, lambda_, epsilon, A, h, mask=False):
     """
     # omega = omega * h
     # kernel = kernel * h
-    k1_x, k1_y = oscillator_2D(x, y, kernel, omega, lambda_, epsilon, A, mask)
-    k2_x, k2_y = oscillator_2D(x + k1_x * 0.5 * h, y + k1_y * 0.5 * h, kernel, omega, lambda_, epsilon, A, mask)
-    k3_x, k3_y = oscillator_2D(x + k2_x * 0.5 * h, y + k2_y * 0.5 * h, kernel, omega, lambda_, epsilon, A, mask)
-    k4_x, k4_y = oscillator_2D(x + k3_x * h, y + k3_y * h, kernel, omega, lambda_, epsilon, A, mask)
-    new_x, new_y = x + h / 6 * (k1_x + 2 * k2_x + 2 * k3_x + k4_x), y + h / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)
+    k1_x, k1_y = oscillator_2D(x, y, **kwargs)
+    k2_x, k2_y = oscillator_2D(x + k1_x * 0.5 * h, y + k1_y * 0.5 * h, **kwargs)
+    k3_x, k3_y = oscillator_2D(x + k2_x * 0.5 * h, y + k2_y * 0.5 * h, **kwargs)
+    k4_x, k4_y = oscillator_2D(x + k3_x * h, y + k3_y * h, **kwargs)
+    new_x = x + h / 6 * (k1_x + 2 * k2_x + 2 * k3_x + k4_x)
+    new_y = y + h / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y)
+
     return new_x, new_y
 
 
-def oscillator_2D_RK4(x, y, kernel, omega, lambda_, epsilon, A, h, step_count, save_step, mask=False):
+def oscillator_2D_RK4(x, y, h, step_count, save_step, **kwargs):
     """"Solve oscillator_2D by Runge–Kutta method.
 
     Args:
         x: array of x coordinates
         y: array of y coordinates
-        kernel: [description]
-        omega: angular velocity
-        A: Standard state amplitude
         h: Step width
         step_count: Number of calculations
         save_step: Storage frequency(Save all if one.)
@@ -99,11 +101,23 @@ def oscillator_2D_RK4(x, y, kernel, omega, lambda_, epsilon, A, h, step_count, s
             save = save + 1
             if save >= len(save_idx):
                 break
-        x, y = RK4(x, y, kernel, omega, lambda_, epsilon, A, h, mask)
+        x, y = RK4(x, y, h, **kwargs)
     return xs, ys
 
 
-def xy2theta_amp(xs, ys, save="twin2d", kernel=0, omega=0, lambda_=0, epsilon=0, A=0, h=0, step_count=0, save_step=0):
+def xy2theta_amp_save(
+    xs,
+    ys,
+    save="twin2d",
+    kernel=0,
+    omega=0,
+    lambda_=0,
+    epsilon=0,
+    A=0,
+    h=0,
+    step_count=0,
+    save_step=0,
+):
     """Function to convert Cartesian coordinates to polar coordinates.
 
     Args:
@@ -120,31 +134,65 @@ def xy2theta_amp(xs, ys, save="twin2d", kernel=0, omega=0, lambda_=0, epsilon=0,
     xy = cp.stack([xs, ys])
     r = cp.linalg.norm(xy, axis=0)
     if save is not False:  # save something
-        dirctroy = os.path.join(save, datetime.datetime.today().strftime("%y%m%d%H%M"))
+        dirctroy = os.path.join(save, datetime.datetime.today().strftime("%y%m%d"))
         if not os.path.exists(dirctroy):
             os.makedirs(dirctroy)
         cp.save(os.path.join(dirctroy, "theta.npy"), theta)
         cp.save(os.path.join(dirctroy, "r.npy"), r)
-        # save theta by color
+        # 位相を色相に変換する．振幅が0のときは黒(明度0)にする．
+        theta[r == 0] = -1
         color = im.make_colors(cp.asnumpy(theta), black=-1)
-        im.save_imgs(save_folder=dirctroy, img=color, file_name='color_theta', stack=True)
+        im.save_imgs(
+            save_folder=dirctroy, img=color, file_name="color_theta", stack=True
+        )
         # save r by color
         r01 = cp.copy(r)
+        print(r01.dtype)
         r_max, r_min = cp.max(r), cp.min(r[r != 0])
         r01 = (r01 - r_min) / (r_max - r_min) * 0.7
         r01[r == 0] = -1
         color = im.make_colors(cp.asnumpy(r01), black=-1)
-        im.save_imgs(save_folder=dirctroy, img=color, file_name='color_r' + str(r_max) + '-' + str(r_min), stack=True)
+        im.save_imgs(
+            save_folder=dirctroy,
+            img=color,
+            file_name="color_r" + str(r_max) + "-" + str(r_min),
+            stack=True,
+        )
         # save parameter
-        parameter = {'lambda': lambda_, 'epsilon': epsilon, 'A': A, 'h': h, 'step_count': step_count, 'save_step': save_step}
-        pd.DataFrame(parameter, index=['value']).to_csv(os.path.join(dirctroy, 'parameter.csv'))
-        kernel = np.savetxt(os.path.join(dirctroy, 'kernel.csv'), cp.asnumpy(kernel.reshape(kernel.shape[2], kernel.shape[3])), delimiter=',')
-        np.savetxt(os.path.join(dirctroy, 'omega.csv'), cp.asnumpy(omega), delimiter=',')
+        parameter = {
+            "lambda": lambda_,
+            "epsilon": epsilon,
+            "A": A,
+            "h": h,
+            "step_count": step_count,
+            "save_step": save_step,
+        }
+        pd.DataFrame(parameter, index=["value"]).to_csv(
+            os.path.join(dirctroy, "parameter.csv")
+        )
+        kernel = np.savetxt(
+            os.path.join(dirctroy, "kernel.csv"),
+            cp.asnumpy(kernel.reshape(kernel.shape[2:4])),
+            delimiter=",",
+        )
+        np.savetxt(
+            os.path.join(dirctroy, "omega.csv"), cp.asnumpy(omega), delimiter=","
+        )
     return theta, r
 
 
-if __name__ == '__main__':
-    mask = os.path.join("/hdd1", "Users", "kenya", "Labo", "keisan", "python", "result", "twist_2d", "frond_form_85.png")
+if __name__ == "__main__":
+    mask = os.path.join(
+        "/hdd1",
+        "Users",
+        "kenya",
+        "Labo",
+        "keisan",
+        "python",
+        "result",
+        "twist_2d",
+        "frond_form_85.png",
+    )
     mask = cv2.imread(mask, cv2.IMREAD_GRAYSCALE)
     # R = 250  # 解析のサイズ
     # C = 150  # 解析のサイズ
@@ -152,22 +200,57 @@ if __name__ == '__main__':
     # 細胞サイズを20μ，フロンドサイズを3mm * 5mm とした時．
     A = 1  # amplitude
     lambda_ = 0.02
-    epsilon = -0.005  # twist h
-    omega = 2 * cp.pi / 24 + 0.01 * cp.random.randn(R, C)  # 角速度
+    epsilon = -0.04  # twist h
+    omega = 2 * cp.pi / 25.5 + 0.01 * cp.random.randn(R, C)  # 角速度
     theta = cp.random.rand(R, C) * 0  # 初期位相
-    x = cp.cos(theta).astype(cp.float64) * 1
-    y = cp.sin(theta).astype(cp.float64) * 1
+    x = cp.cos(theta).astype(cp.float64) * 15
+    y = cp.sin(theta).astype(cp.float64) * 15
     kernel = cp.ones((3, 3), dtype=cp.float64) * 0.05
     kernel[1, 1] = 0
     kernel = kernel.reshape(1, 1, kernel.shape[0], kernel.shape[1])
     h = 0.5 ** 5  # 時間刻み huer
     save_step = int(1 / h)  # 保存頻度
-    step_count = int(30 * 24 / h)  # ループ回数
+    step_count = int(10 * 24 / h)  # ループ回数
 
     if mask is not False:
         x[mask == 0] = 0
         y[mask == 0] = 0
     for i in range(1):
-        save = os.path.join("/hdd1", "Users", "kenya", "Labo", "keisan", "python", "result", "twist_2d", "omega-rand")
-        xs, ys = oscillator_2D_RK4(x, y, kernel, omega, lambda_, epsilon, A, h, step_count, save_step, mask=mask)
-        xy2theta_amp(xs, ys, save=save, kernel=kernel, omega=omega, lambda_=lambda_, epsilon=epsilon, A=A, h=h, step_count=step_count, save_step=save_step)
+        save = os.path.join(
+            "/hdd1",
+            "Users",
+            "kenya",
+            "Labo",
+            "keisan",
+            "python",
+            "result",
+            "twist_2d",
+            "omega-rand",
+        )
+        xs, ys = oscillator_2D_RK4(
+            x,
+            y,
+            kernel=kernel,
+            omega=omega,
+            lambda_=lambda_,
+            epsilon=epsilon,
+            A=A,
+            h=h,
+            step_count=step_count,
+            save_step=save_step,
+            mask=mask,
+        )
+        xy2theta_amp_save(
+            xs,
+            ys,
+            save=save,
+            kernel=kernel,
+            omega=omega,
+            lambda_=lambda_,
+            epsilon=epsilon,
+            A=A,
+            h=h,
+            step_count=step_count,
+            save_step=save_step,
+        )
+
